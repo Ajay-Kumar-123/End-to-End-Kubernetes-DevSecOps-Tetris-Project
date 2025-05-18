b62105518f3c51300b9ec0262783b83cfa85a5bd
Ajay <ajayshastri04@gmail.com>
2025-05-18 16:31:25 +0530

    Changes to EKS-TF

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -1,55 +1,110 @@
-data "aws_vpc" "vpc" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc-name]
+resource "aws_vpc" "vpc" {
+  cidr_block = var.eks-vpc-cidr
+
+  tags = {
+    Name = var.eks-vpc-name
   }
 }
 
-data "aws_internet_gateway" "igw" {
-  filter {
-    name   = "tag:Name"
-    values = [var.igw-name]
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = var.eks-vpc-igw-name
   }
 }
 
-data "aws_subnet" "subnet" {
-  filter {
-    name   = "tag:Name"
-    values = [var.subnet-name]
-  }
-}
 
-data "aws_security_group" "sg-default" {
-  filter {
-    name   = "tag:Name"
-    values = [var.security-group-name]
-  }
-}
-
-resource "aws_subnet" "public-subnet2" {
-  vpc_id                  = data.aws_vpc.vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-south-2b"
+#Create the public subnets
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = var.eks-subnet-1-cidr
+  availability_zone = var.eks-subnet-1-az
   map_public_ip_on_launch = true
 
   tags = {
-    Name = var.subnet-name2
+    Name = var.eks-subnet-1-name
   }
 }
 
-resource "aws_route_table" "rt2" {
-  vpc_id = data.aws_vpc.vpc.id
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.eks-subnet-2-cidr
+  availability_zone       = var.eks-subnet-2-az
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.eks-subnet-2-name
+  }
+}
+
+
+resource "aws_route_table" "rt-1" {
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = data.aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Name = var.rt-name2
+    Name = var.rt-1-name
   }
 }
 
-resource "aws_route_table_association" "rt-association2" {
-  route_table_id = aws_route_table.rt2.id
-  subnet_id      = aws_subnet.public-subnet2.id
+resource "aws_route_table" "rt-2" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = var.rt-2-name
+  }
+}
+
+
+resource "aws_route_table_association" "rt-association-1" {
+  route_table_id = aws_route_table.rt-1.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
+
+resource "aws_route_table_association" "rt-association-2" {
+  route_table_id = aws_route_table.rt-2.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
+
+
+resource "aws_security_group" "sg" {
+  name = "EKS-SG"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description = "SSH-Port"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "HTTP Port"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All outbound traffic"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.eks-sg-name
+  }
 }
